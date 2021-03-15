35196bc6532f9d2d41258622bbb5cc35cd299f8f
AmitBudhori07 <amitbudhori07@gmail.com>
2021-03-15 21:41:33 +0530

    solution-fasts

--- pages/api/db/index.js
+++ pages/api/db/index.js
@@ -7,9 +7,8 @@ const database = process.env.DB_DB;
 const password = process.env.DB_PASS;
 const port = process.env.DB_PORT;
 
-const connectionstring = 'postgresql://u5ysmxa3pesckqy7yci7:RmiiVgQvbBOSh3ECJOYg@b7uwu0h7i8pszyogvvnh-postgresql.services.clever-cloud.com:5432/b7uwu0h7i8pszyogvvnh'
-
-const pool = new Pool({connectionstring,}); 
+const pool = new Pool({user,host,database,password,port});
+ 
 module.exports = {
   query: (text, params) => pool.query(text, params)
   }
