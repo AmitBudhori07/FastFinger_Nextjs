c2f9ea29ab13f6539fa8df6fb581d4e75585da65
AmitBudhori07 <amitbudhori07@gmail.com>
2021-03-15 22:14:19 +0530

    solution-f

--- pages/api/db/index.js
+++ pages/api/db/index.js
@@ -1,13 +1,22 @@
 const {Pool,Client} = require('pg');
 /* const {user,host,database,password,port} = require('../Secrets/db.configration');
  */
-const user = `${process.env.DB_USER}`;
+/* const user = `${process.env.DB_USER}`;
 const host = `${process.env.DB_HOST}`;
 const database = `${process.env.DB_DB}`;
 const password = `${process.env.DB_PASS}`;
-const port = `${process.env.DB_PORT}`;
+const port = `${process.env.DB_PORT}`; */
+var config = {
+  user: 'u5ysmxa3pesckqy7yci7',
+  database: 'b7uwu0h7i8pszyogvvnh',
+  password: 'RmiiVgQvbBOSh3ECJOYg',
+  host: 'b7uwu0h7i8pszyogvvnh-postgresql.services.clever-cloud.com',
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 30000
+};
 
-const pool = new Pool({user,host,database,password,port});
+const pool = new Pool(config);
  
 module.exports = {
   query: (text, params) => pool.query(text, params)
