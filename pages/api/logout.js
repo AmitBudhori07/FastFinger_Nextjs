5ba82916736b6e117679624d1d8e2ce3004fe6b9
AmitBudhori07 <amitbudhori07@gmail.com>
2021-03-16 19:30:09 +0530

    solution-fastfingers10

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -3,6 +3,6 @@ import withSession from 'pages/api/db/session'
 export default withSession(async (req, res) => {
   console.log("Destroying")
   req.session.destroy();
-  console.log(request.session.get('user'))
+  console.log(req.session.get('user'))
   res.json({ isLoggedIn: false })
  })
