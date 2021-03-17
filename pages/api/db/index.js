40574cf2a055154482ea721a6c30f7c788af0c4c
AmitBudhori07 <amitbudhori07@gmail.com>
2021-03-17 14:12:00 +0530

    solution-fastfingers28

--- pages/api/db/index.js
+++ pages/api/db/index.js
@@ -1,12 +1,12 @@
 const {Pool,Client} = require('pg');
 /* const {user,host,database,password,port} = require('../Secrets/db.configration');
  */
-/* const user = `${process.env.DB_USER}`;
+ const user = `${process.env.DB_USER}`;
 const host = `${process.env.DB_HOST}`;
 const database = `${process.env.DB_DB}`;
 const password = `${process.env.DB_PASS}`;
-const port = `${process.env.DB_PORT}`; */
-var config = {
+const port = `${process.env.DB_PORT}`; 
+/* var config = {
   user: 'u5ysmxa3pesckqy7yci7',
   database: 'b7uwu0h7i8pszyogvvnh',
   password: 'RmiiVgQvbBOSh3ECJOYg',
@@ -14,11 +14,11 @@ var config = {
   port: 5432,
   max: 10,
   idleTimeoutMillis: 30000
-};
+}; */
 
 /* const connectionString = process.env.CONNECTION_URL
  */
-const pool = new Pool(config);
+const pool = new Pool({user,host,database,password,port});
  
 module.exports = {
   query: (text, params) => pool.query(text, params)
